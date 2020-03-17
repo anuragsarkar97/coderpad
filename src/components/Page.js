207a00c7c6f5ee3efa12ed6816c28a956165b69b
Anurag Sarkar <anurag.sarkar@oyorooms.com>
2020-03-18 00:40:15 +0530

    new stable

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,14 +3,37 @@ import Paper from '../components/Paper.js';
 import { Row, Col, Skeleton, message } from 'antd';
 
 
-const v = {
-  company_name : "facebook",
-  question_tag : ["array", "dp"],
-  question_difficulty : "hard",
-  question_base : "some basic desc ...",
-  full_question : "some markdown content will be available here .... "
-}
+const t = [
+  {
+    company_name : "facebook",
+    question_tag : ["array", "dp"],
+    question_difficulty : "hard",
+    question_base : "find the longest subsequence ",
+    full_question : "some markdown content will be available here .... "
+  },
+  {
+    company_name : "bloomberg",
+    question_tag : ["2 pointer", "dp"],
+    question_difficulty : "hard",
+    question_base : "find the smallest set ",
+    full_question : "some markdown content will be available here .... "
+  },
+  {
+    company_name : "google",
+    question_tag : ["matrix", "dfs"],
+    question_difficulty : "hard",
+    question_base : "find islands ",
+    full_question : "some markdown content will be available here .... "
+  },
+  {
+    company_name : "facebook",
+    question_tag : ["bfs", "sorting"],
+    question_difficulty : "hard",
+    question_base : "find the correct letter ",
+    full_question : "some markdown content will be available here .... "
+  }
 
+];
 
 export default class Page extends Component {
 
@@ -27,13 +50,13 @@ export default class Page extends Component {
 
   cards() {
     let rows = [];
-    for(var i=0;i<20;i++) {
+    t.map((item, index) => {
       rows.push(<Col span={8}>
         <Skeleton active loading={this.state.loading}>
-          <Paper question={v} />
+          <Paper question={item} />
           </Skeleton>
           </Col>); 
-    } 
+    })
     return rows;
   }
 
--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,24 +1,21 @@
 import React, {
     Component
 } from 'react'
-import Card from '@material-ui/core/Card';
+
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import MonacoEditor from 'react-monaco-editor';
 import {
-    Row,
-    Col,
     Drawer,
     Dropdown,
     Menu,
     BackTop,
     Rate,
-    Divider
+    Card,
 } from 'antd';
-import {HeartTwoTone} from '@ant-design/icons';
+import {HeartTwoTone, BookOutlined, EditOutlined, EllipsisOutlined, SettingOutlined, QuestionOutlined, BookFilled } from '@ant-design/icons';
 import Tags from '../components/Tags';
+
 
 export default class Paper extends Component {
 
@@ -32,11 +29,13 @@ export default class Paper extends Component {
             placementB: 'right',
             visibleB: false,
             selectOnLineNumbers: true,
-            selectedLanguage: "python"
+            selectedLanguage: "python",
+            marked: false
         }
     }
 
     showDrawerA = () => {
+        console.log("p");
         this.setState({
             visibleA: true
         });
@@ -71,91 +70,72 @@ export default class Paper extends Component {
         });
     }
 
+    handleBookmark = () => {
+        this.setState({marked : !this.state.marked});
+    }
+
 
     render() {
 
         let v = this.props.question;
 
-        let menu = ( <Menu onClick = {this.handleMenuClick} >
-            <Menu.Item > python </Menu.Item> 
-            <Menu.Item >java </Menu.Item > 
-            <Menu.Item> cpp </Menu.Item > 
+        let menu = ( <Menu onClick = {this.handleMenuClick}>
+            <Menu.Item >python</Menu.Item> 
+            <Menu.Item >java</Menu.Item > 
+            <Menu.Item>cpp</Menu.Item > 
             </Menu >
             );
+
+
 
             return ( 
             <div> 
                 <BackTop /> 
-                <Card className = "paper" variant = "outlined"
+                <Card title={v.company_name}
+                // extra={<Icon type="message"  theme="outlined" />}
+                actions={[
+                    <QuestionOutlined key="question" onClick={this.showDrawerA} />,
+                    <EditOutlined key="solve" onClick={this.showDrawerB} />,
+                    <Rate key="rate" />,
+                  ]}
                 style = {{
                         marginLeft: '2.5%',
                         marginRight: '2.5%',
                         marginTop: '2.5%'
-                    }}>
-                    <CardContent>
-                        <Typography className = "question" color = "h2" gutterBottom> 
-                        {v.company_name} 
-                        </Typography> 
-                        <Tags tag = {v.question_tag} /> 
-                        <Typography variant = "body2" component = "p" > 
-                        {v.question_base} 
-                        </Typography> 
-                    </CardContent> 
-                    <Row> 
-                        <Col> 
-                            <CardActions> 
-                                <Row> 
-                                    <Col span = {24}> 
-                                    < Button size = "small" onClick = {this.showDrawerB}> Solve </Button>
-                                    </Col> 
-                                    <Drawer 
-                                    title = "Coder Pad" 
-                                    width = {720} 
-                                    placement = {this.state.placementB} 
-                                    closable = {true} 
-                                    onClose = {this.onCloseB} 
-                                    visible = {this.state.visibleB} 
-                                    footer = 
-                                        { 
-                                        <div style = {{display: 'flex',justifyContent: 'space-between'}} >
-                                        <Dropdown overlay = {menu}>
-                                        <Button style = {{leftMargin: '5%'}}> {this.state.selectedLanguage} </Button>
-                                        </Dropdown> 
-                                        <Button onClick = {() => {alert("clicked")}} > Submit </Button>  
-                                        </div>}> 
-                                        <MonacoEditor 
-                                            language = {this.state.selectedLanguage}
-                                            theme = "vs-light"
-                                            value = {this.state.code}
-                                            options = {this.state.options}
-                                            onChange = {this.onChange}
-                                            editorDidMount = {this.editorDidMount} />   
-                                    </Drawer> 
-                                </Row > 
-                            </CardActions> 
-                        </Col> 
-                        <Col >
-                            <CardActions >
-                                <Row >
-                                    <Col span = {24}> 
-                                        <Button size = "small" onClick = {this.showDrawerA}> 
-                                        Learn More 
-                                        </Button> 
-                                    </Col> 
-                                    <Drawer title = "Question Board"
-                                    placement = {this.state.placementA}
-                                    closable = {false}
-                                    onClose = {this.onCloseA}
-                                    visible = {this.state.visibleA}> 
-                                    {v.full_question} 
-                                    </Drawer> 
-                                </Row> 
-                            </CardActions> 
-                        </Col> 
-                        <Col>
-                        <Divider />
-                        </Col>
-                    </Row> 
+                    }} >
+                        <p>
+                            {v.question_base}
+                        </p>
+                    <Drawer title = "Question Board"
+                        placement = {this.state.placementA}
+                        closable = {true}
+                        onClose = {this.onCloseA}
+                        visible = {this.state.visibleA}> 
+                        {v.full_question} 
+                    </Drawer> 
+                    <Drawer 
+                        title = "Coder Pad" 
+                        width = {720} 
+                        placement = {this.state.placementB} 
+                        closable = {true} 
+                        onClose = {this.onCloseB} 
+                        visible = {this.state.visibleB} 
+                        footer = 
+                            { 
+                            <div style = {{display: 'flex',justifyContent: 'space-between'}} >
+                            <Dropdown overlay = {menu}>
+                            <Button style = {{leftMargin: '5%'}}> {this.state.selectedLanguage} </Button>
+                            </Dropdown> 
+                            <Button onClick = {() => {alert("clicked")}} > Submit </Button>  
+                            </div>}> 
+                            <MonacoEditor 
+                                language = {this.state.selectedLanguage}
+                                theme = "vs-light"
+                                value = {this.state.code}
+                                options = {this.state.options}
+                                onChange = {this.onChange}
+                                editorDidMount = {this.editorDidMount} />   
+                        </Drawer> 
                 </Card> 
             </div>
             )
