58b08d225c5632f70c496dffef561530c99fe859
Anish Kumar Saha <anishkumar.saha@traxtech.com>
2020-04-23 00:50:53 +0530

    scaled css added

--- frontent/src/components/SignIn.js
+++ frontent/src/components/SignIn.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
-import { Row, Col } from 'antd';
+import { Row, Col,Card, Button } from 'antd';
+//import Card from 'antd';
 import TopImage from './assets/undraw_programming_2svr.svg';
-
+import DsaImage from './assets/undraw_software_engineer_lvl5.svg';
+import WebDevImage from './assets/undraw_web_developer_p3e5.svg';
+import MobileAppImage from './assets/undraw_Mobile_application_mr4r.svg';
+import MLImage from './assets/undraw_Artificial_intelligence_oyxx.svg';
+import MSLogo from './assets/mslogo.svg';
+import AmazonLogo from './assets/amazonsvglogo.svg'
+import OyoLogo from './assets/oyosvglogo.svg'
+import GoogleLogo from './assets/googlesvglogo.svg'
+import DeshawLogo from './assets/deshawsvglogo.svg'
+import AppleLogo from './assets/applesvglogo.svg'
+import FedexLogo from './assets/fedxsvglogo.svg'
+import ReactRotatingText from 'react-rotating-text'
+import Image from 'react-image-resizer';
 export default class SignIn extends Component {
 
   constructor(props){
@@ -58,30 +71,133 @@ export default class SignIn extends Component {
       //       Welcome to the competition !
       //    </div> */}
          <Row>
-          <Col span={12}>
-            <img src={TopImage} style={{maxWidth : '90%', maxheight : '120%',marginLeft : '5%', marginTop: '5%'}} />
-          </Col>
-          <Col span={12} style={{fontFamily:'Oswald, sans-serif;', fontWeight:'bold',fontSize:'30px', marginTop:'10%'}}>
-            Are you updated with the latest tech ?
+          <Col span={24} style={{backgroundImage:'linear-gradient(to right, #9147FF, #BE40FF,#D441FD'}}>
+            <div class='introText' style={{fontFamily:'Oswald, sans-serif;', fontWeight:'bold', marginTop:'10%', textAlign:'center',
+          fontSize:'40px', color:'white'}}>
+              Learn 
+              <ReactRotatingText items={[' Data Structures & Algorithms', ' Web Development', ' Mobile App Development']}/>
+            </div>
+            <div class='summary' style={{fontFamily:'Manrope, sans-serif', marginTop:'1%', textAlign:'center', fontSize:'20px'
+          ,color:'white'}}>
+              Coding is not difficult if taught in a fun manner !
+            </div>
+            <div class='btn1' style={{marginTop:'10%', marginBottom:'5%', textAlign:'center'}}>
+              <Button color="95389e" size="sm">
+                Join us !
+              </Button>{' '}
+            </div>
           </Col>
         </Row>
         <Row>
-          <Col style={{fontFamily:'Oswald, sans-serif;', fontWeight:'bold',fontSize:'20px', marginTop:'5%',marginBottom:'5%',
-                    display:'flex', justifyContent:'center', alignItems:'center'}}>
-            So many things to learn....
+          <Col span={24}>
+           <div class='courseTitle' style={{fontFamily:'Oswald, sans-serif;', fontWeight:'bold',fontSize:'20px', marginTop:'1%',marginBottom:'1%',
+                    display:'flex', justifyContent:'center', alignItems:'center', textAlign:'center'}}>
+            What do we offer ?
+            </div>
           </Col>
         </Row>
         <Row>
-          <Col span={6}>
-
+          <Col span={6} >
+          <div class ='dsaImage' style={{marginLeft:'10%', marginRight:'10%'}}>
+            <Card style={{width:'100%',border:"2px solid #BE40FF", marginBottom:'10px'}}>
+              < img src={DsaImage} height='70%' width='90%' class ='center'/>
+              <p class='dsaHeader' style={{marginLeft:'10%', marginRight:'10%', fontFamily:'Oswald, sans-serif;',
+              fontWeight:'bold', fontSize:'10px',marginTop:'10%', display:'block'}}>
+              DS & ALGORITHMS
+              </p>
+              <p class='dsaSummary'>
+                We'll cover the fundamentals topics like Arrays,Linked Lists, Stacks & Queues, Trees
+                & many more to skyrocket your chances of getting desriable job interviews.
+              </p>
+            </Card>    
+          </div>
+        
           </Col>
           <Col span={6}>
-
+          
+            <div class='webDevImage' style={{marginLeft:'10%', marginRight:'10%'}}>
+            <Card style={{width:'100%',border:"2px solid #BE40FF", marginBottom:'10px'}}>
+              <img src = {WebDevImage} height='60%' width='90%' class='center'/>
+              <p class='webDevHeader' style={{marginLeft:'10%', marginRight:'10%', fontFamily:'Oswald, sans-serif;',
+              fontWeight:'bold', fontSize:'10px', marginTop:'10%', display:'block'}}>
+              WEB DEVELOPMENT
+              </p>
+              <p class='webDevSummary'>
+                A long, long, long paragraph about all the ins and outs of Full Stack Engineering. A long, long, long paragraph about all the ins and outs of Full Stack Engineering.
+              </p>
+              </Card>
+            </div>
           </Col>
           <Col span={6}>
-
+            <div class='mobileAppImage' style={{marginLeft:'10%', marginRight:'10%'}}>
+            <Card style={{width:'100%',border:"2px solid #BE40FF", marginBottom:'10px'}}>
+            <img src = {MobileAppImage} height='60%' width='90%'/>
+            <p class='appDevHeader' style={{marginLeft:'10%', marginRight:'10%', fontFamily:'Oswald, sans-serif;',
+              fontWeight:'bold', fontSize:'10px', marginTop:'13%', display:'block'}}>
+              MOBILE APP DEVELOPMENT
+              </p>
+              <p class='appDevSummary'>
+                A long, long, long paragraph about all the ins and outs of Mobile App Engineering.
+                A long, long, long paragraph about all the ins and outs of Mobile App Engineering.
+              </p>
+              </Card>
+            </div>
           </Col>
           <Col span={6}>
+          <div class='mlImage' style={{marginLeft:'10%', marginRight:'10%'}}>
+          <Card style={{width:'100%',border:"2px solid #BE40FF", marginBottom:'10px'}}>
+            <img src = {MLImage} height='40%' width='90%'/>
+            <p class='mlHeader' style={{marginLeft:'10%', marginRight:'10%', fontFamily:'Oswald, sans-serif;',
+              fontWeight:'bold', fontSize:'10px',marginTop:'10%', display:'block',align:'center'}}>
+              MACHINE LEARNING
+              </p>
+              <p class='mlSummary'>
+                A long, long, long paragraph about all the ins and outs of Machine Learning. A long, long, long paragraph about all the ins and outs of Machine Learning.
+              </p>
+              </Card>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+          <div class='instructorsHeader' style={{fontFamily:'Manrope, sans-serif;',
+            textAlign:'center', color:'#646965', fontSize:'25px', marginTop:'2%'}}>
+              Instructors from companies like....
+            </div>
+          </Col>
+        </Row>
+        <Row style={{backgroundColor:'white', color:'grey'}}>
+          <Col span={8}>
+            <div class='MicrosoftLogo' style={{marginLeft:'10%',marginRight:'10%',marginTop:'20%'}}>
+              <img src={MSLogo} width='90%' height='90%'>
+
+              </img>
+            </div>
+            <div class='GoogleLogo' style={{marginLeft:'10%',marginRight:'10%',marginTop:'20%'}}>
+              <img src={GoogleLogo} width='90%' height='90%'>
+
+              </img>
+            </div>
+          </Col>
+          <Col span={8}>
+            <div class='AmazonLogo' style={{marginLeft:'5%',marginRight:'5%', marginTop:"15%"}}>
+              <img src={AmazonLogo} width='90%' height='90%'>
+              </img>
+            </div>
+            <div class='FedexLogo' style={{marginLeft:'24%',marginRight:'5%', marginTop:"20%"}}>
+              <img src={FedexLogo} width='60%' height='60%'>
+              </img>
+            </div>
+          </Col>
+          <Col span={8}>
+          <div class='OyoLogo' style={{marginLeft:'5%',marginRight:'5%',marginTop:'10%'}}>
+              <img src={OyoLogo} width='90%' height='90%'>
+              </img>
+            </div>
+            <div class='AppleLogo' style={{marginLeft:'35%',marginRight:'5%',marginTop:'7%'}}>
+              <img src={AppleLogo} width='50%' height='50%'>
+              </img>
+            </div>
           </Col>
         </Row>
        </div>
